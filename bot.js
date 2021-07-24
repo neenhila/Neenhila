bec9a5b57e2a10f1a06053e4398871ba20ad61cf
alejandro1x <83518175+alejandro1x@users.noreply.github.com>
2021-07-24 17:23:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,11 @@ disbut(client);
 
 client.login(process.env.TOKEN);
 
+client.on("ready", () => {
+     client.user.setActivity(`${client.guilds.size} sunucuda hizmet vermekte!`, {type: "PLAYING"});
+}
+)
+
 async function getGuild(id){
     try{
         data = await guildSettings.findOne({
