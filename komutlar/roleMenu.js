ad71645952750230a39903d3f9296523f939cb54
alejandro1x <83518175+alejandro1x@users.noreply.github.com>
2021-07-26 00:49:27 +0300

    Update roleMenu.js

--- komutlar/roleMenu.js
+++ komutlar/roleMenu.js
@@ -8,6 +8,7 @@ module.exports = {
     execute(message){
                 if(message.guild.id !== "868618227310280744") return;
                 if(message.author.bot) return;
+                message.delete({timeout:3000});
         
                 let menuOption1 = new disbut.MessageMenuOption()
                 .setValue("rol1")
@@ -32,46 +33,7 @@ module.exports = {
                 .setID("menu")
                 .setPlaceholder("Tıkla ve rolünü al! / Click and get a role!")
         
-                message.channel.send("Bir seçenek seç. / Choose an option.", menu).then(msg => {
-                    client.on("clickMenu", async menu => {
-                        await menu.clicker.fetch();
-                        if(menu.clicker.id !== message.author.id) return menu.clicker.user.send("Lütfen kendiniz !rolemenu kullanarak menüyü kullanın. Başkalarının menüsünü meşgul etmeyin. / Please use your !rolemenu menu. Don't use others menu, bot's going busy.")
-                        await menu.reply.think(true)
-                        if(menu.values[0] === "rol1"){
-                            if(menu.clicker.member.roles.cache.has("868966251253342269")){
-                                menu.clicker.member.roles.remove(`868966251253342269`)
-                                menu.reply.edit(`Başarıyla <@&868966251253342269> rolünü üzerinizden aldım. / I took <@&868966251253342269> role from you.`)
-                            } else {
-                            menu.clicker.member.roles.add("868966251253342269")
-                            menu.reply.edit(`Başarıyla <@&868966251253342269> rolünü aldınız. / I gave you <@&868966251253342269> role.`)
-                            }
-                        } else if(menu.values[0] === "rol2"){
-                            if(menu.clicker.member.roles.cache.has("868966260401127494")){
-                                menu.clicker.member.roles.remove(`868966260401127494`)
-                                menu.reply.edit(`Başarıyla <@&868966260401127494> rolünü üzerinizden aldım. / I took <@&868966260401127494> role from you.`)
-                            } else {
-                            menu.clicker.member.roles.add("868966260401127494")
-                            menu.reply.edit(`Başarıyla <@&868966260401127494> rolünü aldınız. / I gave you <@&868966260401127494> role.`)
-                            }
-                        } else if(menu.values[0] === "rol3"){
-                            if(menu.clicker.member.roles.cache.has("868966260908625930")){
-                                menu.clicker.member.roles.remove(`868966260908625930`)
-                                menu.reply.edit(`Başarıyla <@&868966260908625930> rolünü üzerinizden aldım. / I took <@&868966260908625930> role from you.`)
-                            } else {
-                            menu.clicker.member.roles.add("868966260908625930")
-                            menu.reply.edit(`Başarıyla <@&868966260908625930> rolünü aldınız. / I gave you <@&868966260908625930> role.`)
-                            }
-                        }else if(menu.values[0] === "rol4"){
-                            if(menu.clicker.member.roles.cache.has("868966261604909056")){
-                                menu.clicker.member.roles.remove(`868966261604909056`)
-                                menu.reply.edit(`Başarıyla <@&868966261604909056> rolünü üzerinizden aldım. / I took <@&868966261604909056> role from you.`)
-                            } else {
-                            menu.clicker.member.roles.add("868966261604909056")
-                            menu.reply.edit(`Başarıyla <@&868966261604909056> rolünü aldınız. / I gave you <@&868966261604909056> role.`)
-                            }
-                        }
-                    })
-                })
+                message.channel.send("Bir seçenek seç. / Choose an option.", menu)
         
             }
 }
