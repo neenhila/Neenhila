1c5666a4849a2e081d7a5d2b6670a9d45f416f9e
alejandro1x <83518175+alejandro1x@users.noreply.github.com>
2021-07-26 00:29:50 +0300

    Add files via upload

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const guildSettings = require("../modeller/guildSettings.js")
+module.exports = {
+    name: "guildMemberRemove",
+    description: "Birisi sunucudan ayrıldığında çalışır.",
+    once: false,
+    execute(member){
+        let memberSize = member.guild.memberCount;
+        if(!data.goodbyeCh){
+            member.guild.channels.cache.forEach(x => {
+                if(x.type = "text"){
+                    if(x.permissionsFor(member.guild.me).has("SEND_MESSAGES")){
+                        defaultChannel = x;
+                    }
+                }
+            })
+            defaultChannel.send(`Güle güle kanalı ayarlı değil. **Ayarlamak için \`"${data.prefix}welcome"\` yazıp boşluk bırakarak __kanalı etiketleyin!__** **${member.user.username}** aramızdan ayrıldı.`)
+        } else if(data.goodbyeCh){
+            member.guild.channels.cache.get(`${data.goodbyeCh}`).send(`${member.user.username} aramızdan ayrıldı! Artık ${memberSize} kişiyiz!`)
+        }
+    }
+}
