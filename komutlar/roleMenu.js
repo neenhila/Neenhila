b9242c69931d750c504bb6f3b97597f49a700a60
alejandro1x <83518175+alejandro1x@users.noreply.github.com>
2021-07-26 00:30:06 +0300

    Add files via upload

---
+++ komutlar/roleMenu.js
@@ -0,0 +1,73 @@
+module.exports = {
+    name: "roleMenu",
+    usage: "rolemenu",
+    description: "Rolleri menü üzerinden almanızı sağlar.",
+    execute(message){
+                if(message.author.bot) return;
+        
+                let menuOption1 = new disbut.MessageMenuOption()
+                .setValue("rol1")
+                .setLabel("> Support")
+                
+                let menuOption2 = new disbut.MessageMenuOption()
+                .setValue("rol2")
+                .setLabel("> Chatting")
+        
+                let menuOption3 = new disbut.MessageMenuOption()
+                .setValue("rol3")
+                .setLabel("> Learning")
+        
+                let menuOption4 = new disbut.MessageMenuOption()
+                .setValue("rol4")
+                .setLabel("> Fun")
+        
+                let menu = new disbut.MessageMenu()
+                .addOptions(menuOption1, menuOption2, menuOption3, menuOption4)
+                .setMinValues(1)
+                .setMaxValues(1)
+                .setID("menu")
+                .setPlaceholder("Tıkla ve rolünü al! / Click and get a role!")
+        
+                message.channel.send("Bir seçenek seç. / Choose an option.", menu).then(msg => {
+                    client.on("clickMenu", async menu => {
+                        await menu.clicker.fetch();
+                        if(menu.clicker.id !== message.author.id) return menu.clicker.user.send("Lütfen kendiniz !rolemenu kullanarak menüyü kullanın. Başkalarının menüsünü meşgul etmeyin. / Please use your !rolemenu menu. Don't use others menu, bot's going busy.")
+                        await menu.reply.think(true)
+                        if(menu.values[0] === "rol1"){
+                            if(menu.clicker.member.roles.cache.has("868966251253342269")){
+                                menu.clicker.member.roles.remove(`868966251253342269`)
+                                menu.reply.edit(`Başarıyla <@&868966251253342269> rolünü üzerinizden aldım. / I took <@&868966251253342269> role from you.`)
+                            } else {
+                            menu.clicker.member.roles.add("868966251253342269")
+                            menu.reply.edit(`Başarıyla <@&868966251253342269> rolünü aldınız. / I gave you <@&868966251253342269> role.`)
+                            }
+                        } else if(menu.values[0] === "rol2"){
+                            if(menu.clicker.member.roles.cache.has("868966260401127494")){
+                                menu.clicker.member.roles.remove(`868966260401127494`)
+                                menu.reply.edit(`Başarıyla <@&868966260401127494> rolünü üzerinizden aldım. / I took <@&868966260401127494> role from you.`)
+                            } else {
+                            menu.clicker.member.roles.add("868966260401127494")
+                            menu.reply.edit(`Başarıyla <@&868966260401127494> rolünü aldınız. / I gave you <@&868966260401127494> role.`)
+                            }
+                        } else if(menu.values[0] === "rol3"){
+                            if(menu.clicker.member.roles.cache.has("868966260908625930")){
+                                menu.clicker.member.roles.remove(`868966260908625930`)
+                                menu.reply.edit(`Başarıyla <@&868966260908625930> rolünü üzerinizden aldım. / I took <@&868966260908625930> role from you.`)
+                            } else {
+                            menu.clicker.member.roles.add("868966260908625930")
+                            menu.reply.edit(`Başarıyla <@&868966260908625930> rolünü aldınız. / I gave you <@&868966260908625930> role.`)
+                            }
+                        }else if(menu.values[0] === "rol4"){
+                            if(menu.clicker.member.roles.cache.has("868966261604909056")){
+                                menu.clicker.member.roles.remove(`868966261604909056`)
+                                menu.reply.edit(`Başarıyla <@&868966261604909056> rolünü üzerinizden aldım. / I took <@&868966261604909056> role from you.`)
+                            } else {
+                            menu.clicker.member.roles.add("868966261604909056")
+                            menu.reply.edit(`Başarıyla <@&868966261604909056> rolünü aldınız. / I gave you <@&868966261604909056> role.`)
+                            }
+                        }
+                    })
+                })
+        
+            }
+}
---
+++ komutlar/setupGoodbye.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const guildSettings = require("../modeller/guildSettings.js");
+module.exports = {
+    name: "setupGoodbye",
+    usage: "goodbye",
+    description: "Goodbye kanalını ayarlamanızı sağlar.",
+    execute(message, prefix){
+        if(!message.member.hasPermission("ADMINISTRATOR")) return;
+        const args = message.content.slice().split(" ");
+        const etiketCh = message.mentions.channels.first();
+        if(!etiketCh){
+            return message.reply(`Değiştirilecek kanalı etiketlemediniz! **Şu anki güle güle kanalınız: \`${data.goodbyeCh}\`** `);
+        } else {
+            guildSettings.findOne({
+                id: message.guild.id,
+            }).then(guild1 => {
+                guild1.goodbyeCh = `${etiketCh.id}`
+                guild1.save().catch(err => console.log(err))
+            })
+            message.channel.send(`Yeni hoşgeldin kanalınız <#${etiketCh.id}> olarak ayarlandı!`);
+        }
+    }
+    
+}
