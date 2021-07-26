e66fe583e196155de12573f51502faf1c21c6641
alejandro1x <83518175+alejandro1x@users.noreply.github.com>
2021-07-26 19:26:49 +0300

    Update ping.js

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -4,9 +4,9 @@ module.exports = {
     description: "Botun pingini gösterir.",
     cooldown: 20000,
     execute(message, client, Discord){
-        if(client.ws.ping > 100) {color = "RED"} else {color = "GREEN"}
+        if(client.ws.ping > 150) {color = "RED"} else {color = "GREEN"}
         let emb = new Discord.MessageEmbed()
-        .setDescription(`\`${client.ws.ping}\` ping ile hizmet vermekteyim.`)
+        .setDescription(`**${client.user.tag}** olarak \`${client.ws.ping}\` ping ile hizmet vermekteyim.`)
         .setColor(color)
 
         message.channel.send(emb);
