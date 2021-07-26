d85f796b5a53f56fa1d786b754716dbe971fb2c5
alejandro1x <83518175+alejandro1x@users.noreply.github.com>
2021-07-26 22:59:10 +0300

    Update stats.js

--- komutlar/stats.js
+++ komutlar/stats.js
@@ -12,11 +12,11 @@ module.exports = {
             .setDescription(`Merhaba, bu bot tarafımca (Neenhila#0666) hazırlanan ilk public botudur. Elbette eksileri olacaktır. Bunları bana bildirmekten çekinmeyin. Bota __**!öneri boşluk mesajınızı**__ yazarak bana iletebilirsiniz.\n\n`)
             .setThumbnail(client.user.displayAvatarURL({dynmaic: true}))
             .addFields(
-                {name: "<a:RainbowOkGif:869301898946158763> Bot Pingi", value:`  >>> || ${msg.createdTimestamp - message.createdTimestamp} ||`, inline: true},
+                {name: "<a:RainbowOkGif:869301898946158763> Bot Pingi", value:`  >>> \`${msg.createdTimestamp - message.createdTimestamp}\` ms`, inline: true},
             
-                {name: "<a:RainbowOkGif:869301898946158763> Discord API Pingi", value:`  >>>  || ${client.ws.ping} ||`, inline: true},
+                {name: "<a:RainbowOkGif:869301898946158763> Discord API Pingi", value:`  >>> \`${client.ws.ping}\` ms`, inline: true},
 
-                {name: "<a:SabitGif:869301913089355857> Bot kaç sunucuda aktif?", value:`>>> || ${client.guilds.cache.size} ||`, inline: false},
+                {name: "<a:SabitGif:869301913089355857> Bot kaç sunucuda aktif?", value:`>>> \`${client.guilds.cache.size}\` guild(s)`, inline: false},
 
                 {name: "<a:AyarGif:869301910853795921> Botun prefixini nasıl ayarlarım?", value:">>> **!prefix [prefixiniz]** yaparak botun prefixini değiştirebilirsiniz!", inline: false},
 
