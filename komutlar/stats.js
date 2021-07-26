e0a0425b5d945772af78649d98a8fb6bf9fec450
alejandro1x <83518175+alejandro1x@users.noreply.github.com>
2021-07-26 22:54:01 +0300

    Create stats.js

---
+++ komutlar/stats.js
@@ -0,0 +1,32 @@
+module.exports = {
+    name: "stats",
+    usage: "stats",
+    cooldown: 20000,
+    description: "Sunucunun statlarını gösterir.",
+    execute(message, client, Discord){
+        message.channel.send("Data yükleniyor.").then(msg => {
+            msg.delete();
+
+            let embed = new Discord.MessageEmbed()
+            .setAuthor(`Neenhila Discord Bot İstatistikleri`, client.user.displayAvatarURL({dynamic: true}))
+            .setDescription(`Merhaba, bu bot tarafımca (Neenhila#0666) hazırlanan ilk public botudur. Elbette eksileri olacaktır. Bunları bana bildirmekten çekinmeyin. Bota __**!öneri boşluk mesajınızı**__ yazarak bana iletebilirsiniz.\n\n`)
+            .setThumbnail(client.user.displayAvatarURL({dynmaic: true}))
+            .addFields(
+                {name: "<a:RainbowOkGif:869301898946158763> Bot Pingi", value:`  >>> || ${msg.createdTimestamp - message.createdTimestamp} ||`, inline: true},
+            
+                {name: "<a:RainbowOkGif:869301898946158763> Discord API Pingi", value:`  >>>  || ${client.ws.ping} ||`, inline: true},
+
+                {name: "<a:SabitGif:869301913089355857> Bot kaç sunucuda aktif?", value:`>>> || ${client.guilds.cache.size} ||`, inline: false},
+
+                {name: "<a:AyarGif:869301910853795921> Botun prefixini nasıl ayarlarım?", value:">>> **!prefix [prefixiniz]** yaparak botun prefixini değiştirebilirsiniz!", inline: false},
+
+                {name: "<a:AyarGif:869301910853795921> Botun temel komutları nelerdir?", value:">>> **!welcome** ile hoşgeldin kanalınızı ayarlayabilirsiniz.\n**!goodbye** ile güle güle kanalını ayarlayabilirsiniz.\n**!help** ile yardım menüsünden detaylı bilgilere ulaşabilirsiniz.", inline: false}
+
+            )
+            .setFooter(`${message.author.tag} tarafından istendi!`, message.author.displayAvatarURL({dynmaic: true}))
+            .setTimestamp()
+            
+            message.channel.send(embed)
+        })
+    }
+}
