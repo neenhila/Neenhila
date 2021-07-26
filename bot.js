a8aef85d8f78a59e39c8f7141f23f6771de89c71
alejandro1x <83518175+alejandro1x@users.noreply.github.com>
2021-07-26 17:52:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -174,3 +174,20 @@ if(message.guild.id !== "868618227310280744") return;
 }
      })
 })
+
+client.on("message", message => {
+  const args = message.content.split(" ").slice(1);
+
+  if (message.content.startsWith(config.prefix + "eval")) {
+    if(message.author.id !== config.ownerID) return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${err}\n\`\`\``);
+    }
+  }
+});
