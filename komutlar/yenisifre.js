4c77d15aead0c459376ee38d159a9ef1fbe233ef
alejandro1x <83518175+alejandro1x@users.noreply.github.com>
2021-07-31 17:28:10 +0300

    Update yenisifre.js

--- komutlar/yenisifre.js
+++ komutlar/yenisifre.js
@@ -8,7 +8,10 @@ module.exports = {
             var randomChars = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789*-/+,%&![{}]()`;
             var pwLen = 9;
             var randPw = Array(pwLen).fill(randomChars).map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
+if(message.guild) {
             message.author.send(`Yeni şifreniz: \` ${randPw} \` `)
             message.channel.send(`Şifreniz DM kutunuza gönderilmiştir. Son derece güçlü ve güvenilir bir şifredir. Şifreniz kurucularım dahil hiçkimse ile paylaşılmamaktadır!`)
+} else {
+    message.channel.send(`Yeni şifreniz: \` ${randPw} \` `);
         }
     }
