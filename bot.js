4a613d199fece675c7f8ee2c11b322ed84ecb440
alejandro1x <83518175+alejandro1x@users.noreply.github.com>
2021-07-31 17:32:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,7 +81,7 @@ for (const file of commandFiles) {
 
 
 client.on("message", async message => {
-    if(!message.guild) { message.guild.id === "868618227310280744"};
+    if(!message.guild) { g1 = "868618227310280744"} else { g1 = message.guild.id };
     await getGuild(message.guild.id).then(() => {
         if (!message.content.startsWith(data.prefix) || message.author.bot) return;
         const args = message.content.slice(data.prefix.length).trim().split(/ +/);
